336073c7c9937be6965e5d12d6dcb91eb07220cc
Daisy <49625347+wangbiy@users.noreply.github.com>
2019-06-02 19:10:19 +0800

    Update contact.h

--- test_2019_5_17_1/test_2019_5_17_1/contact.h
+++ test_2019_5_17_1/test_2019_5_17_1/contact.h
@@ -1,22 +1,12 @@
-//创建一个通讯录
-//1000个人的信息
-//每个人的信息：姓名、年龄、电话、住址、性别
-
-//1.添加人的信息
-//2.删除人的信息
-//3.修改人的信息
-//4.查找信息
-//5.安排xxx排序
-//6.打印通讯录的信息
-//0.退出
-#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
-#define MAX_NAME 20
-#define MAX_SEX 5
-#define MAX_TELE 12
-#define MAX_ADDR 30
+#define MAX_Name 20
+#define MAX_Sex 3
+#define MAX_Tele 12
+#define MAX_Addr 30
 #define MAX 1000
 enum Option
 {
@@ -26,27 +16,27 @@ enum Option
 	SEARCH,
 	MODIFY,
 	SHOW,
-	SORT
+	SORT,
+	EMPTY
 };
-typedef struct PeoInfo
+typedef struct Info
 {
-	char name[MAX_NAME];
+	char name[MAX_Name];
 	int age;
-	char sex[MAX_SEX];
-	char tele[MAX_TELE];
-	char addr[MAX_ADDR];
-}PeoInfo;
-//通讯录结构
+	char sex[MAX_Sex];
+	char tele[MAX_Tele];
+	char addr[MAX_Addr];
+}Info;
 typedef struct Contact
 {
-	struct PeoInfo data[MAX];//1000个人的信息
-	int sz;//记录当前有多少个人的信息
+	int sz;
+	struct Info data[MAX];
 }Contact;
-void InitContact(Contact* pcon);
-void AddContact(Contact* pcon);
-void ShowContact(const Contact* pcon);
-void DelContact(Contact* pcon);
-void SearchContact(const Contact* pcon);
-void ModifyContact(Contact* pcon);
-void SortContact(Contact* pcon);
+void InitContact(Contact *pcon);
+void AddContact(Contact *pcon);
+void ShowContact(const Contact *pcon);
+void DelContact(Contact *pcon);
+void SearchContact(const Contact *pcon);
+void ModifyContact(Contact *pcon);
+void SortContact(Contact *pcon);
 void EmptyContact(Contact *pcon);
